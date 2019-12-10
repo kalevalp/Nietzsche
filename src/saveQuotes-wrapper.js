be7e31c656940411616e704ed333292b375ddefb
Kalev Alpernas <kalev.alp@gmail.com>
2019-12-10 17:25:47 +0200

    Running with wrappers.

---
+++ src/saveQuotes-wrapper.js
@@ -0,0 +1,10 @@
+const recorder = require('watchtower-recorder');
+
+// Loading modules that fail when required via vm2
+const aws = require('aws-sdk');
+
+const mock = {
+    'aws-sdk' : aws,
+};
+
+module.exports.handler = recorder.createRecordingHandler('src/saveQuotes.js', 'handler', mock, false, () => {}, true);
