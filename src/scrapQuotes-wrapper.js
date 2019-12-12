6576d28d29a2fe04448545fa482f3e1f704baf5b
Kalev Alpernas <kalev.alp@gmail.com>
2019-12-12 17:23:37 +0200

    recording events

--- src/scrapQuotes-wrapper.js
+++ src/scrapQuotes-wrapper.js
@@ -1,13 +1,40 @@
 const recorder = require('watchtower-recorder');
+const eventsStreamName = process.env['WATCHTOWER_EVENT_KINESIS_STREAM'];
+const batchEventPublisher = recorder.createBatchEventPublisher(eventsStreamName);
+const debug = process.env.DEBUG_WATCHTOWER;
+
+// Unfortunately, need to recreate the id hashing logic from the code.
+// This type of dependence seems to be unavoidable at times.
+const crypto = require('crypto')
+const md5 = str =>
+  crypto
+    .createHash('md5')
+    .update(str)
+    .digest('hex')
 
 // Loading modules that fail when required via vm2
 const aws = require('aws-sdk');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
+
+const gru = require('./utils/goodreads')
+
+let context, lambdaExecutionContext, lambdaInputEvent;
+function updateContext(name, event, lambdaContext) { context = name; lambdaExecutionContext = lambdaContext; lambdaInputEvent = event; }
+
 const mock = {
     'aws-sdk' : aws,
     'request-promise': rp,
     'cheerio': cheerio,
+    './utils/goodreads': {
+        fetchQuotes: (...args) => {
+            gru.fetchQuotes(...args)
+                .then(quotes => {
+                    const logEvents = quotes.map(quote => ({name: "SCRAPED_QUOTE", params: {quoteID: md5(`${quote.author}-${quote.text}`)}}));
+                    batchEventPublisher(logEvents,lambdaExecutionContext);
+                })
+        },
+    }
 };
 
-module.exports.handler = recorder.createRecordingHandler('src/scrapQuotes.js', 'handler', mock, false, () => {}, true);
+module.exports.handler = recorder.createRecordingHandler('src/scrapQuotes.js', 'handler', mock, false, updateContext, true);
--- src/tweetQuoteText-wrapper.js
+++ src/tweetQuoteText-wrapper.js
@@ -1,14 +1,24 @@
 const recorder = require('watchtower-recorder');
+const eventsStreamName = process.env['WATCHTOWER_EVENT_KINESIS_STREAM'];
+const eventPublisher = recorder.createEventPublisher(eventsStreamName);
+const debug = process.env.DEBUG_WATCHTOWER;
 
 // Loading modules that fail when required via vm2
 const aws = require('aws-sdk');
 const request = require('request');
 const rp = require('request-promise');
 
+let context, lambdaExecutionContext, lambdaInputEvent;
+function updateContext(name, event, lambdaContext) { context = name; lambdaExecutionContext = lambdaContext; lambdaInputEvent = event; }
+
+const twitConditions = [{ cond: () => true, 
+                          opInSucc: (argumentsList) => (response) => eventPublisher({name: "PUBLISHED_TWEET", params: {quoteID: lambdaInputEvent.quoteId}})}]
+
 const mock = {
     'aws-sdk' : aws,
     'request' : request,
     'request-promise' : rp,
+    'twit' : recorder.createTwitMock([]),
 };
 
-module.exports.handler = recorder.createRecordingHandler('src/tweetQuoteText.js', 'handler', mock, false, () => {}, true);
+module.exports.handler = recorder.createRecordingHandler('src/tweetQuoteText.js', 'handler', mock, false, updateContext, true);
